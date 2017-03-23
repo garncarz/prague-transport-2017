0325efca8737092e504da07245c7f062511f78c3
Petr Holasek <holasekp@gmail.com>
2017-03-23 11:24:21 +0100

    task1: standalone binary made and moved to task1/

--- Makefile
+++
@@ -1,7 +0,0 @@
-all: compute_hub
-
-find_hub.o: find_hub.cc find_hub.hpp
-	g++ -Wall -c find_hub.cc
-
-compute_hub: find_hub.o
-	g++ -Wall compute_hub.cc find_hub.o -o compute_hub
--- task1/find_hub.cc
+++ task1/find_hub.cc
@@ -1,5 +1,15 @@
 #include <iostream>
+#include <vector>
 #include "find_hub.hpp"
+
+struct Result find_hub(int cities_count, int offers_count, struct Offer *offers)
+{
+    struct Result res;
+
+    std::cout << offers_count << std::endl;
+
+    return res;
+}
 
 int main(int argc, char *argv[])
 {
