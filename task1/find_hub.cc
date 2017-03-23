ffaa3d619e2bbe45e26a5d39b2de888c1e3f8645
Petr Holasek <holasekp@gmail.com>
2017-03-24 00:54:35 +0100

    find_hub: endmonds algorithm used for searching of branchings

--- task1/find_hub.cc
+++ task1/find_hub.cc
@@ -1,30 +1,60 @@
-#include <iostream>
-#include <vector>
 #include "find_hub.hpp"
 
-struct Result find_hub(int cities_count, int offers_count, struct Offer *offers)
+void GraphProcessor::find_hub()
 {
-    struct Result res;
+    struct Result res = {};
 
-    std::cout << offers_count << std::endl;
+    complete_graph      g(cities_cnt);
+    multi_array<int, 2> weights(extents[cities_cnt][cities_cnt]);
+    Vertex              roots[] = {0, 1, 2, 3};
+    vector<Edge>        branching;
 
-    return res;
+    weights[0][1] = 6;
+    weights[1][2] = 10;
+    weights[2][1] = 10;
+    weights[1][3] = 12;
+    weights[3][2] = 8;
+    weights[3][0] = 1;
+
+    edmonds_optimum_branching<false, true, true>
+            (g, identity_property_map(), weights.origin(),
+             roots, roots, back_inserter(branching));
+
+    BOOST_FOREACH (Edge e, branching)
+    {
+        std::cout << source(e, g) << " -> " << target(e, g) << std::endl;
+    }
+
+    last_res = res;
+}
+
+void GraphProcessor::load_input()
+{
+    cities_cnt = 4;
+    lines.push_back(Line(0, 1, 6));
+    lines.push_back(Line(1, 2, 10));
+    lines.push_back(Line(2, 1, 10));
+    lines.push_back(Line(1, 3, 12));
+    lines.push_back(Line(3, 2, 8));
+    lines.push_back(Line(3, 0, 1));
+}
+
+void GraphProcessor::send_result()
+{
+    std::cout << last_res.feasibility << " " << last_res.total_cost << " " << last_res.depot_id << std::endl;
+
+    for (auto it = last_res.rec_offers.begin(); it != last_res.rec_offers.end(); ++it) {
+        std::cout << it->from << " " << it->to << " " << it->price << std::endl;
+    }
 }
 
 int main(int argc, char *argv[])
 {
-    int cities_cnt = 4;
-    struct Offer offers[] = {
-                            {0, 1, 6},
-                            {1, 2, 10},
-                            {2, 1, 10},
-                            {1, 3, 12},
-                            {3, 2, 8},
-                            {3, 0, 1}
-                        };
-    struct Result res;
+    GraphProcessor gp;
 
-    res = find_hub(cities_cnt, sizeof(offers)/sizeof(struct Offer), offers);
+    gp.load_input();
+    gp.find_hub();
+    //gp.send_result();
 
     return 0;
 }
--- task1/find_hub.hpp
+++ task1/find_hub.hpp
@@ -1,18 +1,122 @@
-struct Offer {
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <iterator>
+#include <cstdlib>
+#include <limits>
+#include <cerrno>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/graph_traits.hpp>
+#include <boost/graph/graph_concepts.hpp>
+#include <boost/concept_check.hpp>
+#include <boost/operators.hpp>
+#include <boost/iterator.hpp>
+#include <boost/multi_array.hpp>
+#include <boost/random.hpp>
+#include "edmonds_optimum_branching.hpp"
+
+using namespace std;
+using namespace boost;
+
+struct Line {
     unsigned int from, to, price;
+
+    Line(unsigned int from, unsigned int to, unsigned int price) : from(from), to(to), price(price) {};
+};
+
+struct City {
+    unsigned int num;
+    std::vector<Line> in;
+    std::vector<Line> out;
 };
 
 struct Result {
     bool feasibility;
     unsigned int total_cost;
     unsigned int depot_id;
-    unsigned int recommended_offers_num;
-    struct Offer *recommended_offers;
-
-    Result() : recommended_offers_num(0), recommended_offers(NULL) {}
-    ~Result() {::free(recommended_offers); }
+    std::vector<Line> rec_offers;
 };
 
-typedef struct Offer Conn;
+class GraphProcessor {
+public:
+    void load_input();
+    void find_hub();
+    void send_result();
 
-struct Result find_hub(int cities_count, int offers_count, struct Offer *offers);
+private:
+    Result last_res;
+    int cities_cnt;
+    std::vector<Line> lines;
+};
+
+// definitions of a complete graph that implements the EdgeListGraph
+// concept of Boost's graph library.
+namespace boost {
+    struct complete_graph {
+        complete_graph(int n_vertices) : n_vertices(n_vertices) {}
+        int n_vertices;
+
+        struct edge_iterator : public input_iterator_helper<edge_iterator, int, std::ptrdiff_t, int const *, int>
+        {
+            int edge_idx, n_vertices;
+
+            edge_iterator() : edge_idx(0), n_vertices(-1) {}
+            edge_iterator(int n_vertices, int edge_idx) : edge_idx(edge_idx), n_vertices(n_vertices) {}
+            edge_iterator &operator++()
+            {
+                if (edge_idx >= n_vertices * n_vertices)
+                    return *this;
+                ++edge_idx;
+                if (edge_idx / n_vertices == edge_idx % n_vertices)
+                    ++edge_idx;
+                return *this;
+            }
+            int operator*() const {return edge_idx;}
+            bool operator==(const edge_iterator &iter) const
+            {
+                return edge_idx == iter.edge_idx;
+            }
+        };
+    };
+
+    template<>
+    struct graph_traits<complete_graph> {
+        typedef int                             vertex_descriptor;
+        typedef int                             edge_descriptor;
+        typedef directed_tag                    directed_category;
+        typedef disallow_parallel_edge_tag      edge_parallel_category;
+        typedef edge_list_graph_tag             traversal_category;
+        typedef complete_graph::edge_iterator   edge_iterator;
+        typedef unsigned                        edges_size_type;
+
+        static vertex_descriptor null_vertex() {return -1;}
+    };
+
+    pair<complete_graph::edge_iterator, complete_graph::edge_iterator>
+    edges(const complete_graph &g)
+    {
+        return make_pair(complete_graph::edge_iterator(g.n_vertices, 1),
+                         complete_graph::edge_iterator(g.n_vertices, g.n_vertices*g.n_vertices));
+    }
+
+    unsigned
+    num_edges(const complete_graph &g)
+    {
+        return (g.n_vertices - 1) * (g.n_vertices - 1);
+    }
+
+    int
+    source(int edge, const complete_graph &g)
+    {
+        return edge / g.n_vertices;
+    }
+
+    int
+    target(int edge, const complete_graph &g)
+    {
+        return edge % g.n_vertices;
+    }
+}
+
+typedef graph_traits<complete_graph>::edge_descriptor Edge;
+typedef graph_traits<complete_graph>::vertex_descriptor Vertex;
