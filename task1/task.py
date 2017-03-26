f78fbaf454a19b06fdd76d3e20fbc43768536cfd
Ondřej Garncarz <ondrej@garncarz.cz>
2017-03-26 22:22:22 +0200

    #4: Task 1 solver bound.

---
+++ task1/task.py
@@ -0,0 +1,45 @@
+import re
+import subprocess
+
+from main import celery
+
+
+def dict_to_stdin(d):
+    s = '%d\n' % d['citiesCount']
+    for offer in d['costOffers']:
+        s += '%d %d %d\n' % (offer['from'], offer['to'], offer['price'])
+    return s.encode()
+
+
+def stdout_to_dict(b):
+    d = {}
+    m = re.split(r'[ \t\n\r:]+', b.decode())
+
+    d['feasible'] = True if m[0] == '1' else False
+    d['totalCost'] = int(m[1])
+    d['depotId'] = int(m[2])
+
+    d['recommendedOffers'] = []
+    for i in range(3, len(m) - 1, 3):
+        d['recommendedOffers'].append({
+            'from': int(m[i]),
+            'to': int(m[i + 1]),
+            'price': int(m[i + 2]),
+        })
+
+    return d
+
+
+def _solve(_input):
+    p = subprocess.Popen(['task1/find_hub'],
+                         stdin=subprocess.PIPE,
+                         stdout=subprocess.PIPE)
+    p.stdin.write(dict_to_stdin(_input))
+    p.stdin.close()
+    p.wait()
+    return stdout_to_dict(p.stdout.read())
+
+
+@celery.app.task
+def solve(_input):
+    return _solve(_input)
--- tests/test_task1.py
+++ tests/test_task1.py
@@ -1,47 +1,54 @@
 import json
 
+from deepdiff import DeepDiff
+import pytest
+
 from main.app import app
+from main import settings
+from task1 import task
 
 
-def test_task1():
-    _input = {
-        'citiesCount': 4,
-        'costOffers': [
-            {
-                'from': 0,
-                'to': 1,
-                'price': 6
-            },
-            {
-                'from': 1,
-                'to': 2,
-                'price': 10
-            },
-            {
-                'from': 2,
-                'to': 1,
-                'price': 10
-            },
-            {
-                'from': 1,
-                'to': 3,
-                'price': 12
-            },
-            {
-                'from': 3,
-                'to': 2,
-                'price': 8
-            },
-            {
-                'from': 3,
-                'to': 0,
-                'price': 1
-            },
-        ],
-    }
+with open('tests/task1_input.json') as f:
+    _input = json.loads(f.read())
 
+with open('tests/task1_output.json') as f:
+    _output = json.loads(f.read())
+
+
+@pytest.mark.skipif(not settings.TESTING_API,
+                    reason='At least one Celery worker needed to test API.')
+def test_api():
     response = app.test_client().post('/task1/input', data=json.dumps(_input))
     data = json.loads(response.data.decode())
 
     assert data['feasible'] == True
     assert data['totalCost'] == 15
+
+
+def test_task():
+    result = task._solve(_input)
+    assert not DeepDiff(result, _output, ignore_order=True)
+
+
+def test_dict_to_stdin():
+    result = task.dict_to_stdin(_input)
+    assert result.decode() == '''\
+4
+0 1 6
+1 2 10
+2 1 10
+1 3 12
+3 2 8
+3 0 1
+'''
+
+
+def test_stdout_to_dict():
+    b = '''\
+1 15 3
+0 1 6
+3 0 1
+3 2 8
+'''.encode()
+    result = task.stdout_to_dict(b)
+    assert result == _output
